d6f6852158e1a7416a84f9093ee936e39f634e97
gleblix <190660620+gleblix@users.noreply.github.com>
2024-12-16 07:42:30 -0800

    Create function addHumanChoice and test.
    Create function addHumanChoice which prompts
    user for choice of rock, paper, scissors.

--- paper-rock-scissors/javascript.js
+++ paper-rock-scissors/javascript.js
@@ -4,7 +4,6 @@
 // representing the computer's choice in the game.
 function getComputerChoice() {
     let randomNumber = randomIntFromInterval(1, 3);
-    console.log(randomNumber)
     switch (randomNumber) {
         case 1:
             return 'rock';
@@ -13,10 +12,26 @@ function getComputerChoice() {
         case 3:
             return 'scissors';
     }
+}
 
-    // Returns a random integer from num1 (inclusive) to num2 (inclusive)
-    // Where num1, num2 both positive integers with num1 < num2
-    function randomIntFromInterval (num1, num2) {
-        return Math.floor(Math.random() * (num2 - num1 + 1)) + num1;
+// Returns a random integer from num1 (inclusive) to num2 (inclusive)
+// Where num1, num2 both positive integers with num1 < num2
+function randomIntFromInterval (num1, num2) {
+    return Math.floor(Math.random() * (num2 - num1 + 1)) + num1;
+}
+
+// Sends a prompt to the CLI requesting the user's choice of rock
+// paper, scissors. If the user types a valid choice, that choice is
+// returned (in lowercase). If the user types an invalid choice, 'rock'
+// is returned.
+function getHumanChoice() {
+    let input = prompt("Choose rock, paper, or scissors: ");
+    input = input.toLowerCase();
+
+    if (input == 'rock' || input == 'paper' || input == 'scissors') {
+        return input;
+    }
+    else {
+        return 'rock';
     }
 }
