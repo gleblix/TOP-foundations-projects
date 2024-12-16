6f0bcb536e4c0f09bdd55bd4b5315eab936d0658
gleblix <190660620+gleblix@users.noreply.github.com>
2024-12-16 04:35:19 -0800

    Rock paper scissors - initialize html and java
    Create index.html and javascript.js. Test link
    from javascript to index.html with console.log()
    commands.

---
+++ paper-rock-scissors/javascript.js
@@ -0,0 +1,6 @@
+/* javascript support file for html */
+
+console.log("Test - is this printing to the console?");
+let a = 5;
+let b = 5;
+console.log(`Test - you should see the number 10 here: ${a+b}`);
