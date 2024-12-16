8dda5a97306b908d0fd3e9c6b3eb454cbb9b0e4e
gleblix <190660620+gleblix@users.noreply.github.com>
2024-12-16 08:46:45 -0800

    Create function playRound.
    Create function playRound that resolves
    a single round of the game. Must be hard called
    from the console with lower case valid inputs
    for testing. Prototype.

--- paper-rock-scissors/javascript.js
+++ paper-rock-scissors/javascript.js
@@ -1,5 +1,10 @@
 // javascript support file for html
 
+// Variable that tracks the user's score over one or more games.
+let humanScore = 0;
+// Variable that tracks the computer's score over one or more games.
+let computerScore = 0;
+
 // Returns a random choice of ['rock', 'paper', 'scissors']
 // representing the computer's choice in the game.
 function getComputerChoice() {
@@ -12,12 +17,6 @@ function getComputerChoice() {
         case 3:
             return 'scissors';
     }
-}
-
-// Returns a random integer from num1 (inclusive) to num2 (inclusive)
-// Where num1, num2 both positive integers with num1 < num2
-function randomIntFromInterval (num1, num2) {
-    return Math.floor(Math.random() * (num2 - num1 + 1)) + num1;
 }
 
 // Sends a prompt to the CLI requesting the user's choice of rock
@@ -34,4 +33,82 @@ function getHumanChoice() {
     else {
         return 'rock';
     }
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == 'rock') {
+        switch (computerChoice) {
+            case 'rock':
+                tie();
+                return;
+            case 'paper':
+                computerWins();
+                return;
+            case 'scissors':
+                humanWins();
+                return;
+        }
+    } else if (humanChoice == 'paper') {
+        switch (computerChoice) {
+            case 'rock':
+                humanWins();
+                return;
+            case 'paper':
+                tie();
+                return;
+            case 'scissors':
+                computerWins();
+                return;
+        }
+    } else if (humanChoice == 'scissors') {
+        switch (computerChoice) {
+            case 'rock':
+                computerWins();
+                return;
+            case 'paper':
+                humanWins();
+                return;
+            case 'scissors':
+                tie();
+                return;
+        }
+    }
+
+    function tie() {
+        console.log(`It's a tie! You both chose ${capitalize(humanChoice)}.`);
+        console.log(`Computer Score: ${computerScore}`);
+        console.log(`Human Score: ${humanScore}`);
+        return;
+    }
+
+    function humanWins() {
+        console.log(`You win! ${capitalize(humanChoice)} ` +
+        `beats ${capitalize(computerChoice)}.`);
+        humanScore++;
+        console.log(`Computer Score: ${computerScore}`);
+        console.log(`Human Score: ${humanScore}`);
+        return;
+    }
+
+    function computerWins() {
+        console.log(`The Computer wins! ${capitalize(computerChoice)} ` +
+        `beats ${capitalize(humanChoice)}.`);
+        computerScore++;
+        console.log(`Computer Score: ${computerScore}`);
+        console.log(`Human Score: ${humanScore}`);
+        return;
+    }
+}
+
+//Utility functions
+
+// Returns a random integer from num1 (inclusive) to num2 (inclusive)
+// Where num1, num2 both positive integers with num1 < num2
+function randomIntFromInterval (num1, num2) {
+    return Math.floor(Math.random() * (num2 - num1 + 1)) + num1;
+}
+
+//Capitalizes a word
+function capitalize(string) {
+    return string.slice(0,1).toUpperCase() + string.slice(1);
 }
